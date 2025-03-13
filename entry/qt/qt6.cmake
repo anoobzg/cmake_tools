90f8c2f6d758e6b54e3737ba3991fa430589603f
zenggui <anoob@sina.cn>
2025-03-13 20:12:46 +0800

    qt6

--- entry/qt/qt6.cmake
+++ entry/qt/qt6.cmake
@@ -9,8 +9,9 @@ macro(__enable_qt6)
         
         find_package(Qt${QT_VERSION_MAJOR} COMPONENTS Core Gui Widgets Network Svg REQUIRED)
         find_package(Qt${QT_VERSION_MAJOR} COMPONENTS OpenGL OpenGLWidgets REQUIRED)
+        find_package(Qt${QT_VERSION_MAJOR} COMPONENTS Charts SerialPort REQUIRED)
 
-        set(libs Qt6::Core Qt6::Gui Qt6::Widgets Qt6::Network Qt6::Svg Qt6::OpenGL Qt6::OpenGLWidgets)
+        set(libs Qt6::Core Qt6::Gui Qt6::Widgets Qt6::Network Qt6::Svg Qt6::OpenGL Qt6::OpenGLWidgets Qt6::Charts Qt6::SerialPort)
 
         add_library(external_qt INTERFACE IMPORTED GLOBAL)
         add_library(external_qt::external_qt ALIAS external_qt)
