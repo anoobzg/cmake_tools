f6b8199d716d3dbe9ed6712619ef0a3750c5b135
anoob <anoob@sina.cn>
2025-04-05 11:46:30 +0800

    linux only create Release

---
+++ conan/create/create_linux.py
@@ -0,0 +1,11 @@
+import os
+import util
+
+if __name__ == "__main__":
+    name, version = util.parse_recipe()
+    p = util.search_recipe_path(name, version)
+    
+    print('create conan package : {}'.format(p))
+    util.conan_create(p, version, "Release")
+
+    util.conan_upload(name, version)
