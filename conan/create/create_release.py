ca705e320dba852783b5c443bed4ed14d0ce220d
zenggui <anoob@sina.cn>
2025-10-09 19:08:39 +0800

    feature:debug,release

---
+++ conan/create/create_release.py
@@ -0,0 +1,13 @@
+import os
+import util
+
+if __name__ == "__main__":
+    name, version = util.parse_recipe()
+    p = util.search_recipe_path(name, version)
+    
+    util.modify_yml_file(p, name, version)
+    
+    print('create conan package : {}'.format(p))
+    util.conan_create(p, version, "Release")
+
+    util.conan_upload(name, version)
